ec1ec413b419644e3b9ee57c6770a6c9dd116402
wairimukuria.wk@gmail.com <you@example.com>
2022-06-03 13:25:12 +0300

    Assignment done

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  
+  function logShout(string) {
+    console.log(string.toUpperCase());
+  }
+  function logWhisper (string) {
+    console.log(string.toLowerCase( ));
+  }
+  function sayHiToHeadphonedRoommate (aWord) {
+    var canthear = "I can't hear you!";
+    var yes = "YES INDEED!";
+    var loveto = "I would love to!";
+    var dinner = "Let's have dinner together!";
+    if (aWord.toLowerCase(aWord) === aWord) {
+      return canthear;
+    }
+    else if (aWord.toUpperCase(aWord) === aWord) {
+      return yes;
+    }
+    else if ("Let's have dinner together!" === aWord) {
+      return loveto
+    }
+  }
+    
